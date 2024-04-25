c5b4e2f824a1e5570545392a9ee8cfcfbe5ac45c
jilbenbrito <125512468+jilbenbrito@users.noreply.github.com>
2024-04-24 20:59:50 -0400

    Update pa01.c

--- pa01.c
+++ pa01.c
@@ -1,30 +1,3 @@
-/*============================================================================
-| Assignment: pa01 - Encrypting a plaintext file using the Hill cipher
-|
-| Author: Jilben Brito
-| Language: c
-|
-| To Compile: javac pa01.java
-| gcc -o pa01 pa01.c
-| g++ -o pa01 pa01.cpp
-| go build pa01.go
-|
-| To Execute: java -> java pa01 kX.txt pX.txt
-| or c++ -> ./pa01 kX.txt pX.txt
-| or c -> ./pa01 kX.txt pX.txt
-| or go -> ./pa01 kX.txt pX.txt
-| or python -> python3 pa01.py kX.txt pX.txt
-| where kX.txt is the keytext file
-| and pX.txt is plaintext file
-| Note:
-| All input files are simple 8 bit ASCII input
-| All execute commands above have been tested on Eustis
-|
-| Class: CIS3360 - Security in Computing - Fall 2023
-| Instructor: McAlpin
-| Due Date: 2/25/24
-+===========================================================================*/
-
 //Libraries
 #include <stdio.h>
 #include <stdlib.h>
@@ -215,12 +188,3 @@ void fillCtArr(char ctArr[], int idx, char tempArr[], int kLen){
     }
 
 }
-
-/*=============================================================================
-| I Jilben Brito(5512370) affirm that this program is
-| entirely my own work and that I have neither developed my code together with
-| any another person, nor copied any code from any other person, nor permitted
-| my code to be copied or otherwise used by any other person, nor have I
-| copied, modified, or otherwise used programs created by others. I acknowledge
-| that any violation of the above terms will be treated as academic dishonesty.
-+=============================================================================*/
